f62bcc370f09640d98335dc5aa5b06c336b397a1
bredele <olivier.wietrich@gmail.com>
2018-01-14 18:45:52 -0700

    Generate default OPTIONS

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 
 /**
- * This is a simple description.
+ * Generate the communication options for a manner resource.
  *
+ * @param {Object} resource
+ * @param {Object?} schema
+ * @return {Object}
  * @api public
+ *
+ * @see https://github.com/tether/manner
  */
 
-module.exports = function (resource) {
+module.exports = function (resource, schema) {
   const options = paths(resource)
   return {
     ...resource,
-    options: describe(paths(resource))
+    options: describe(paths(resource, schema))
   }
 }
 
@@ -26,9 +31,31 @@ module.exports = function (resource) {
 function describe (options, schema) {
   const resource = {}
   Object.keys(options).map(path => {
-    resource[path] = () => {}
+    resource[path] = handler(options[paths], {})
   })
   return resource
+}
+
+
+/**
+ * Generate response header according the passed
+ * resource and schema.
+ *
+ * @param {Array} methods
+ * @param {Object} headers
+ * @return {Function}
+ * @api private
+ */
+
+function handler (methods, headers) {
+  return (query, body, req, res) => {
+    res.writeHead(200, {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': `${  methods.map(method => method.toUpperCase()),join(', ')}, OPTIONS`,
+      'Access-Control-Allow-Headers': 'Origin, Content-Type, Authorization, Content-Length, X-Requested-With'
+    })
+    res.end()
+  }
 }
 
 
